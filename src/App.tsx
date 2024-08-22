776222dcfa4618a875954063ea56dda0f032bb0c
Geana Almeida <geana.almeida2000@gmail.com>
2024-08-22 15:25:31 -0300

    hook state e props

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+
+import './App.css'
+import Contador from './pages/contador/Contador'
+import Home from './pages/home/Home'
+import Tarefa from './pages/tarefa/Tarefa'
+
+function App() {
+
+
+  return (
+    <>
+        {/* <Home 
+          titulo='Minha primeira postagem'
+          texto='texto da minha primeira postagem'
+        />
+
+        <Home 
+          titulo='Minha segunda postagem'
+          texto='texto da minha segunda postagem'
+        />   */}
+
+        {/* <Contador/> */}
+
+        <Tarefa/>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/contador/Contador.tsx
@@ -0,0 +1,26 @@
+﻿import { useState } from "react"
+
+function Contador() {
+  
+  //const [nome_estado, função_setState ] = useState(valor_inicial_do_estado)
+
+  const [valor, setValor] = useState(0); //hook state
+
+  function handleClick(){
+    setValor(valor+1)
+  }
+
+  return (
+    <div>
+      <h2>Componente Contador</h2>
+
+      <p>O valor atual do meu estado é: {valor}</p>
+      <button onClick={handleClick}>Adicionar +1</button>
+
+    </div>
+  )
+}
+
+
+
+export default Contador
